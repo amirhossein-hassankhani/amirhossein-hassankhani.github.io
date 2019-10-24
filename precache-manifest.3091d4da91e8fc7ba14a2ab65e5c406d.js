d093a6f45ddf601bb08a9ce4b4c80849359a83e3
Amirhossein Hassankhani <amirhossein.hassankhani1997@gmail.com>
2019-10-24 18:07:19 +0330

    Add files via upload

---
+++ precache-manifest.3091d4da91e8fc7ba14a2ab65e5c406d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3221863bf4a0bab91d96e7d9a3ef1c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6df9c52bee52a6e39e8d",
+    "url": "/static/css/main.4ca671ee.chunk.css"
+  },
+  {
+    "revision": "a3f2799ba4966ea6c7d4",
+    "url": "/static/js/2.f2141cb3.chunk.js"
+  },
+  {
+    "revision": "6df9c52bee52a6e39e8d",
+    "url": "/static/js/main.081cb306.chunk.js"
+  },
+  {
+    "revision": "04163ae6459e6044ac2e",
+    "url": "/static/js/runtime~main.e4cfc818.js"
+  }
+]);
